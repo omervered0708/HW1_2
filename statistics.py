4deb55023d044923b395b91478174b46fce13352
itamar-github <81257975+itamar-github@users.noreply.github.com>
2021-04-06 15:22:41 +0300

    population_statistics' docstring added

--- statistics.py
+++ statistics.py
@@ -2,6 +2,11 @@ from math import ceil
 
 
 def sum(values):
+    """
+    return the sum of values
+    :param values: list of numeric values
+    :return: the sum of values
+    """
     sum=0
     for value in values:
         sum += value
@@ -9,17 +14,42 @@ def sum(values):
 
 
 def mean(values):
+    """
+    return the mean of values
+    :param values: list of numeric values
+    :return: the mean of values
+    """
     return sum(values)/len(values)
 
 
 def median(values):
+    """
+    return the median of values
+    :param values: list of numeric values
+    :return: median of values
+    """
     values.sort()
     n = len(values)
     if n % 2 == 0:
-        med = values[n//2-1] + values[n//2]
+        med = values[(n//2)-1] + values[n//2]
         med /= 2
     else:
         med = values[ceil(n/2)-1]
     return med
 
+
+def population_statistics(feature_description, data, treatment, target, threshold, is_above, statistic_functions):
+    """
+    print statistical indices about data which represents population.
+    the statistical indices are measured on the feature target gathering records by the following rules:
+    if is_above is True - gather records in which treatment's values are greater then or equal to "threshold"
+    :param feature_description: string to describe the name of the set
+    :param data: dictionary which its keys are features from the data set and values are lists of the features' values
+    :param treatment: name of a feature from the data set
+    :param target: name of a feature from the data set
+    :param threshold: threshold value for feature treatment
+    :param is_above: indicator which receives True or False
+    :param statistic_functions: list which contains the statistics methods from statistics.py
+    :return: none
+    """
 
