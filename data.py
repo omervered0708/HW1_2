1971291eea37900d8e268daf7143db3b6d993c3d
omervered0708 <omer.vered@campus.technion.ac.il>
2021-04-07 13:31:28 +0300

    hi

--- data.py
+++ data.py
@@ -11,3 +11,28 @@ def load_data(path, features):
     data = new_data
     return data
 
+
+def filter_by_feature(data, feature, values):
+    data1 = {}
+    data2 = {}
+    init_dict(data1, data)
+    init_dict(data2, data)
+    for i, value in enumerate(data[feature]):
+        if value in values:
+            copy_to_dict(data, i, data1)
+        else:
+            copy_to_dict(data, i, data2)
+    return data1, data2
+
+
+def init_dict(dict, data):
+    for key in data.keys():
+        dict[key] = []
+
+
+def copy_to_dict(data,i,dict):
+    for key, dict_value in data.items():
+        dict[key].append(dict_value[i])
+
+
+
